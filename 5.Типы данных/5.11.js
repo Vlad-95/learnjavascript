05d924699014091f4e68a5176730aacad09b6894
Vladislav <vzv@siluetstudio.com>
2021-11-22 11:16:13 +0300

    files

--- 5.Типы данных/5.11.js
+++ 5.Типы данных/5.11.js
@@ -94,3 +94,137 @@ function getLastDayOfMonth(year, month) {
 }
 
 console.log(getLastDayOfMonth(2021, 1))
+
+
+/*
+6. Сколько сегодня прошло секунд?
+важность: 5
+Напишите функцию getSecondsToday(), возвращающую количество секунд с начала сегодняшнего дня.
+
+Например, если сейчас 10:00, и не было перехода на зимнее/летнее время, то:
+
+getSecondsToday() == 36000 // (3600 * 10)
+Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+*/
+function getSecondsToday() {
+    let now = new Date();
+
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+    let diff = now - today;   
+
+    return  Math.round(diff / 1000)
+}
+
+console.log(getSecondsToday());
+
+
+/*
+7. Сколько секунд осталось до завтра?
+важность: 5
+Создайте функцию getSecondsToTomorrow(), возвращающую количество секунд до завтрашней даты.
+
+Например, если сейчас 23:00, то:
+
+getSecondsToTomorrow() == 3600
+P.S. Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+*/
+
+function getSecondToTomorrow() {
+    let now = new Date();
+
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+    let diff = tomorrow - now;
+
+    return Math.round(diff / 1000);
+}
+
+console.log(getSecondToTomorrow())
+
+/*
+8. Напишите функцию formatDate(date), форматирующую date по следующему принципу:
+
+Если спустя date прошло менее 1 секунды, вывести "прямо сейчас".
+В противном случае, если с date прошло меньше 1 минуты, вывести "n сек. назад".
+В противном случае, если меньше часа, вывести "m мин. назад".
+В противном случае, полная дата в формате "DD.MM.YY HH:mm". А именно: "день.месяц.год часы:минуты", всё в виде двух цифр, т.е. 31.12.16 10:00.
+Например:
+
+alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );
+*/
+
+function formatDate (funcDate) {
+    let diff = new Date() - funcDate; // разница в миллисекундах
+
+    if (diff < 1000) { // меньше 1 секунды
+        return 'прямо сейчас';
+    }
+
+    let sec = Math.floor(diff / 1000); // преобразовать разницу в секунды
+
+    if (sec < 60) {
+        return sec + ' сек. назад';
+    }
+
+    let min = Math.floor(diff / 60000); // преобразовать разницу в минуты
+    if (min < 60) {
+        return min + ' мин. назад';
+    }
+
+    // отформатировать дату
+    // добавить ведущие нули к единственной цифре дню/месяцу/часам/минутам
+    let d = funcDate;
+    d = [
+        '0' + d.getDate(),
+        '0' + (d.getMonth() + 1),
+        '' + d.getFullYear(),
+        '0' + d.getHours(),
+        '0' + d.getMinutes()
+    ].map(component => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
+
+    // соединить компоненты в дату
+    return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+}
+// альтернативное решение
+
+function formatDateAnother (funcDate) {
+    let dayOfMonth = funcDate.getDate();
+    let month = funcDate.getMonth() + 1;
+    let year = funcDate.getFullYear();
+    let hour = funcDate.getHours();
+    let minutes = funcDate.getMinutes();
+    let diffMs = new Date - funcDate;
+    let diffSec = Math.round(diffMs / 1000);
+    let diffMin = diffSec / 60;
+    let diffHour = diffMin / 60;
+
+    //форматирование даты
+    year = year.toString().slice(-2);
+    month = month < 10 ? '0' + month : month;
+    dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+    hour = hour < 10 ? '0' + hour : hour;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    if (diffSec < 1) {
+        return 'прямо сейчас';
+    } else if (diffMin < 1) {
+        return `${diffSec} сек. назад`;
+    } else if (diffHour < 1) {
+        return `${diffMin} мин. назад`;
+    } else {
+        return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
+    }
+}
+
+console.log(formatDate(new Date(new Date - 86400 * 1000)));
+
+
+console.log(formatDateAnother(new Date(new Date - 86400 * 1000)));
